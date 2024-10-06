e5321bb7480473b17d6821596e1cba24b9e2ab0b
Pedro Conde <pepconde.1993@gmail.com>
2024-10-06 19:17:21 +0000

    updated worker bucket url

--- src/constants/environment.const.ts
+++ src/constants/environment.const.ts
@@ -1,5 +1,5 @@
 export const ENVIRONMENT = import.meta.env.PROD ? {
-    workerBucketUrl: 'https://portfolio-bucket-worker.pepconde-1993.workers.dev/'
+    workerBucketUrl: 'https://portfolio-bucket-worker.pepconde-1993.workers.dev'
 } : {
     workerBucketUrl: 'http://localhost:8787'
 };
