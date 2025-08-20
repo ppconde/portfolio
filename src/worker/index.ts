400fcdab0260246c6065a4b3d0e83ad40ab34c95
Pedro Conde <pepconde.1993@gmail.com>
2025-08-20 13:38:32 +0100

    More work on migration

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,25 @@
+import type { R2Bucket } from "@cloudflare/workers-types";
 import { Hono } from "hono";
-const app = new Hono<{ Bindings: Env }>();
 
-app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+type Bindings = {
+  PORTFOLIO_BUCKET: R2Bucket;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.get("*", async (c) => {
+  const key = c.req.path.substring(1);
+  if (!key) return c.text("Bad Request", 400);
+
+  const object = await c.env.PORTFOLIO_BUCKET.get(key);
+  if (!object) return c.text("Not Found", 404);
+
+  const headers: Record<string, string> = {};
+  object.writeHttpMetadata(headers as any);
+  headers["etag"] = object.httpEtag;
+  headers["Cache-Control"] = "public, max-age=3600";
+
+  return new Response(object.body as any, { headers });
+});
 
 export default app;
