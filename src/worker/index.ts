664330f6fba986dfb699b1425cd67e3c5c73ab60
Pedro Conde <pepconde.1993@gmail.com>
2025-08-20 13:49:56 +0100

    chore: fix build

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -2,24 +2,24 @@ import type { R2Bucket } from "@cloudflare/workers-types";
 import { Hono } from "hono";
 
 type Bindings = {
-  PORTFOLIO_BUCKET: R2Bucket;
+	PORTFOLIO_BUCKET: R2Bucket;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("*", async (c) => {
-  const key = c.req.path.substring(1);
-  if (!key) return c.text("Bad Request", 400);
+	const key = c.req.path.substring(1);
+	if (!key) return c.text("Bad Request", 400);
 
-  const object = await c.env.PORTFOLIO_BUCKET.get(key);
-  if (!object) return c.text("Not Found", 404);
+	const object = await c.env.PORTFOLIO_BUCKET.get(key);
+	if (!object) return c.text("Not Found", 404);
 
-  const headers: Record<string, string> = {};
-  object.writeHttpMetadata(headers as any);
-  headers["etag"] = object.httpEtag;
-  headers["Cache-Control"] = "public, max-age=3600";
+	const headers: Record<string, string> = {};
+	object.writeHttpMetadata(headers as any);
+	headers.etag = object.httpEtag;
+	headers["Cache-Control"] = "public, max-age=3600";
 
-  return new Response(object.body as any, { headers });
+	return new Response(object.body as any, { headers });
 });
 
 export default app;
