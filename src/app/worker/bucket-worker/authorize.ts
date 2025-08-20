ec7c0dc35947611e79109466362871076af9a46f
Pedro Conde <pepconde.1993@gmail.com>
2025-08-20 13:38:32 +0100

    More work on migration

--- src/app/worker/bucket-worker/authorize.ts
+++
@@ -1,5 +0,0 @@
-const ALLOW_LIST = [".glb", ".jpg", ".jpeg", ".png", ".gltf", ".bin"];
-
-export function authorizeRequest(request: Request, key: string): boolean {
-	return request.method === "GET" && ALLOW_LIST.some((ext) => new RegExp(`${ext}`, "i").test(key));
-}
