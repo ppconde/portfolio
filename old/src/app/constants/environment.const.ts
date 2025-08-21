c11e5c05ca004133d937b0e058bbd38c7be45d20
Ppconde <pepconde.1993@gmail.com>
2025-08-21 14:52:05 +0100

    chore: remove old

--- old/src/app/constants/environment.const.ts
+++
@@ -1,7 +0,0 @@
-export const ENVIRONMENT = import.meta.env.PROD
-	? {
-			workerBucketUrl: "https://portfolio-bucket-worker.pepconde-1993.workers.dev",
-		}
-	: {
-			workerBucketUrl: "http://localhost:8787",
-		};
